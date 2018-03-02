24d0de5190fec5aea7503831d5894763555e7cf5
Vivek Ramaswamy <vivek_ramaswamy@icloud.com>
2018-03-02 19:50:43 +0530

    Adding sortingWithLambdas changes

--- src/main/java/generics/sort/SortingWithLambdas.java
+++ src/main/java/generics/sort/SortingWithLambdas.java
@@ -1,4 +1,28 @@
 package generics.sort;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class SortingWithLambdas {
+
+    private List<String> stringList= Arrays.asList("This", "is", "a", "list", "of", "string");
+
+
+    //Default sort from Java 7;
+    public List<String> alphaSort() {
+        Collections.sort(stringList);
+        return stringList;
+    }
+
+    //Default sort of Java 8;
+
+    public List<String> alpaSortUsingStream() {
+        return stringList.stream().sorted().collect(toList());
+    }
+
+
 }
